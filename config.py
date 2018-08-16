d55ca8e493628f215365f6999c6d551dafa90f53
luckylion777 <luckylion777>
2018-08-16 12:39:05 -0300

    full commit

---
+++ config.py
@@ -0,0 +1,10 @@
+'''
+Created on Oct 12, 2015
+
+@author: mentzera
+'''
+
+es_host = 'search-es-twitter-demo-md2ezy7ry6byq63ozickyek7ye.us-east-1.es.amazonaws.com'
+es_port = 80
+es_bulk_chunk_size = 1000  #number of documents to index in a single bulk operation
+
